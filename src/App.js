3553002bf8e825e7c2751654c5e5b5988da683fa
Mikołaj Gramowski <emeraldbob020@gmail.com>
2021-05-24 12:09:06 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts'
+
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import { getPosts } from './actions/posts';
 import useStyles from './styles';
 import memories from './images/memories.png';
 
 const App = () => {
-  const classes = useStyles()
-  const dispatch = useDispatch()
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
   useEffect(() => {
- dispatch(getPosts())
-  }, [dispatch])
-    return (
-      <Container maxWidth="lg">
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
@@ -32,7 +36,7 @@ const App = () => {
         </Container>
       </Grow>
     </Container>
-    )
-}
+  );
+};
 
-export default App
+export default App;
