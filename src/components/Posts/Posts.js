8aa2c5be86f6c0e01414528f920a10beeafc8318
Mikołaj Gramowski <emeraldbob020@gmail.com>
2021-05-24 11:23:06 +0200

    forms

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import Post from './Posts/Post'
+import { useSelector } from 'react-redux'
+import Post from './Post/Post';
 import useStyles from './styles';
 const Posts = () => {
+    const posts = useSelector((state) => state.posts)
     const classes = useStyles()
     return (
         <>
